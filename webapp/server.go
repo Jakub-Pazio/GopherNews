12839954a8b045c7acd25b24baa61e1e6b2f3eca
Jakub-Pazio <19jakubpazio99@gmail.com>
2023-11-05 22:33:14 +0100

    Serving fake articles using htmx/echo_v4

--- webapp/server.go
+++ webapp/server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"net/http"
+	// "net/http"
+	"fmt"
+	"html/template"
+	"io"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gopher_news/internal/articles"
 )
 
 func main() {
@@ -14,10 +18,31 @@ func main() {
 	e.Static("/", "./templates")
 	e.Static("/htmx", "./htmx")
 
-	// Later refacot to sever go templates filed with articles data
-	e.GET("/articles", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "<h3>Articles</h3>")
-	})
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
 
+	// Later refactor to sever go templates filed with articles data
+	e.Renderer = t
+	e.GET("/articles", ArticlesList)
 	e.Logger.Fatal(e.Start(":9999"))
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func ArticlesList(c echo.Context) error {
+	// Log that finction was called, TODO: CHANGE THIS
+	fmt.Println("ArticlesList called")
+	// TODO: Fetch articles from database
+	fetchedArticles := []articles.Article{
+		articles.NewArticle("Learn C++", "https://learn-cpp.org", "C++"),
+		articles.NewArticle("Learn Go", "https://learn-go.org", "Go"),
+	}
+	return c.Render(200, "articles", fetchedArticles)
 }
